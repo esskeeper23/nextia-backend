3b5e75b0650371dcecb850b2c31f1c60fd9d1c6d
esskeeper23 <rene2.martinez@outlook.com>
2023-01-11 16:59:25 -0800

    first commit

---
+++ src/middlewares/role.middleware.js
@@ -0,0 +1,9 @@
+const adminValidate = (req, res, next) => {
+    const role = req.user.role
+
+    if (role === 'admin') {
+        next()
+    }else {
+        res.status(401).json({message: 'Access Denied'})
+    }
+}
