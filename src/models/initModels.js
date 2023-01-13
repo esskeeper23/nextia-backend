576e6be1491b369c9242fe50efd4323683406aef
esskeeper23 <rene2.martinez@outlook.com>
2023-01-13 14:52:34 -0800

    typo

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,9 +1,8 @@
-const User = require('./users.model')
 const Invitations = require('./invitations.model')
 const Users = require('./users.model')
 
 const initModels = () => {
-    User.hasMany(Invitations)
+    Users.hasMany(Invitations)
     Invitations.belongsTo(Users)
 }
 
