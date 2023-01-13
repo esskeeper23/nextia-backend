6c157640d096b4e92900a809e298c34ece6ae19e
esskeeper23 <rene2.martinez@outlook.com>
2023-01-13 14:34:13 -0800

    bug fixed

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ app.get('/',(req, res) => {
 
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/auth', authRouter)
-app.use('api/v1/invitations', invitationsRouter)
+app.use('/api/v1/invitations', invitationsRouter)
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`)
